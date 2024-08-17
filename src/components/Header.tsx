477631b86184908035ac02e1834e53b0b76ef0c8
Bernardo Marques <bernardohmarques@gmail.com>
2024-08-17 10:03:41 -0300

    chore: add icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-import { Wrapper } from "./styles/Header.styled";
+import { Minus, Square, X } from "lucide-react";
+import {
+  Actions,
+  CloseContainer,
+  IconContainer,
+  Wrapper,
+} from "./styles/Header.styled";
 
 const Header = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Actions>
+        <IconContainer>
+          <Minus />
+        </IconContainer>
+        <IconContainer>
+          <Square size={10} />
+        </IconContainer>
+        <CloseContainer>
+          <X size={12} strokeWidth={3} />
+        </CloseContainer>
+      </Actions>
+    </Wrapper>
+  );
 };
 
 export default Header;
--- src/components/styles/Header.styled.tsx
+++ src/components/styles/Header.styled.tsx
@@ -4,10 +4,45 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   text-align: center;
-  flex-direction: row;
-  width: 100%;
+  flex-direction: row-reverse;
   height: 24px;
   background-color: #2c2c2c;
-  padding: 0 8px;
+  padding: 4px 12px;
   cursor: default;
+`;
+
+export const IconContainer = styled.div`
+  padding: 2px;
+  width: 14px;
+  height: 14px;
+  transition: 200ms;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    background: #363636;
+  }
+`;
+export const CloseContainer = styled.div`
+  padding: 2px;
+  transition: 200ms;
+  border-radius: 50%;
+  width: 14px;
+  height: 14px;
+  background: #e64f0b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  &:hover {
+    background: #f15f1c;
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
 `;
